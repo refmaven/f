c3ce2c454cc0abc85983219947cd021c98f213c8
refmaven <refmaven@gmail.com>
2024-12-17 14:31:30 +0800

    hello canvas

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
+const canvas = document.getElementById('myCanvas');
+const ctx = canvas.getContext('2d');
 
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+ctx.fillStyle = 'blue';        
+ctx.fillRect(50, 50, 200, 100);
+  
+window.addEventListener('resize', () => {            
+  canvas.width = window.innerWidth;           
+  canvas.height = window.innerHeight;          
+  ctx.fillStyle = 'blue';          
+  ctx.fillRect(50, 50, 200, 100);     
+});
