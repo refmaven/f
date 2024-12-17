f2416c396e9069bf21eeda821acb65be2e94163d
refmaven <riosaktimahendra991@gmail.com>
2024-12-17 14:39:47 +0800

    update main.js

--- main.js
+++ main.js
@@ -1,15 +1,17 @@
-const canvas = document.getElementById('myCanvas');
-const ctx = canvas.getContext('2d');
+window.onload = () => {
+  const canvas = document.getElementById('myCanvas');
+  const ctx = canvas.getContext('2d');
 
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
 
-ctx.fillStyle = 'blue';        
-ctx.fillRect(50, 50, 200, 100);
-  
-window.addEventListener('resize', () => {            
-  canvas.width = window.innerWidth;           
-  canvas.height = window.innerHeight;          
-  ctx.fillStyle = 'blue';          
-  ctx.fillRect(50, 50, 200, 100);     
-});
+  ctx.fillStyle = 'blue';
+  ctx.fillRect(50, 50, 200, 100);
+
+  window.addEventListener('resize', () => {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    ctx.fillStyle = 'blue';
+    ctx.fillRect(50, 50, 200, 100);
+  });
+};
